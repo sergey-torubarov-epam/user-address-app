cc980e8720f3e546677f1a79f4aab2ab8e45b828
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-05-20 18:25:42 -0400

    Convert Address Model to Node.js

--- src/main/java/com/uams/model/Address.java
+++ src/main/java/com/uams/model/Address.java
@@ -1,64 +1,62 @@
-package com.uams.model;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.ToString;
-import lombok.EqualsAndHashCode;
+class Address extends Model {}
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.util.HashSet;
-import java.util.Set;
+Address.init(
+  {
+    addressId: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    buildingName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Street is required',
+        },
+      },
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'City is required',
+        },
+      },
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'State is required',
+        },
+      },
+    },
+    pincode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Pincode is required',
+        },
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Address',
+    tableName: 'addresses',
+    timestamps: false,
+  }
+);
 
-@Entity
-@Table(name = "addresses")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString(exclude = "users")
-@EqualsAndHashCode(exclude = "users")
-public class Address {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "address_id")
-    private Long addressId;
-
-    @Column(name = "building_name")
-    private String buildingName;
-
-    @NotBlank(message = "Street is required")
-    @Column(name = "street", nullable = false)
-    private String street;
-
-    @NotBlank(message = "City is required")
-    @Column(name = "city", nullable = false)
-    private String city;
-
-    @NotBlank(message = "State is required")
-    @Column(name = "state", nullable = false)
-    private String state;
-
-    @NotBlank(message = "Pincode is required")
-    @Column(name = "pincode", nullable = false)
-    private String pincode;
-
-    @ManyToMany(mappedBy = "addresses", fetch = FetchType.LAZY)
-    private Set<User> users = new HashSet<>();
-    
-    // Override toString to prevent infinite recursion
-    @Override
-    public String toString() {
-        return "Address{" +
-                "addressId=" + addressId +
-                ", buildingName='" + buildingName + '\'' +
-                ", street='" + street + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", pincode='" + pincode + '\'' +
-                '}';
-    }
-}
+module.exports = Address;
