f926a77faee5e666bc9a3b672e2e0dc24eb38968
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-05-20 18:45:56 -0400

    Update address model for Node.js project

--- src/models/address.js
+++ src/models/address.js
@@ -26,6 +26,13 @@ const Address = sequelize.define('Address', {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'Users',
+            key: 'id',
+        },
+    },
 });
 
 module.exports = Address;
