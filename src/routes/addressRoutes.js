52c57992deb32bb471b41114bf577a197d79e385
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-05-20 18:46:13 -0400

    Update address routes for Node.js project

--- src/routes/addressRoutes.js
+++ src/routes/addressRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const addressController = require('../controllers/addressController');
 
-router.get('/addresses', addressController.listAddresses);
-router.get('/addresses/new', addressController.showCreateForm);
-router.post('/addresses', addressController.createAddress);
-router.get('/addresses/:id/edit', addressController.showEditForm);
-router.post('/addresses/:id', addressController.updateAddress);
-router.get('/addresses/:id/delete', addressController.deleteAddress);
+router.get('/', addressController.getAllAddresses);
+router.get('/:id', addressController.getAddressById);
+router.post('/', addressController.createAddress);
+router.put('/:id', addressController.updateAddress);
+router.delete('/:id', addressController.deleteAddress);
 
 module.exports = router;
