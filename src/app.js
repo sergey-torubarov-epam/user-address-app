dd735063779f8973e138868ee8a23ec3b7633389
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-05-20 18:45:42 -0400

    Update app.js for Node.js project

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const addressRoutes = require('./routes/addressRoutes');
 const sequelize = require('./config/database');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use('/api', userRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/addresses', addressRoutes);
 
 sequelize.sync().then(() => {
     console.log('Database synced');
