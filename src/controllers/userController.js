618675f8370e00a99687499772d99e2ba03d3c2e
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-05-20 18:46:02 -0400

    Update user controller for Node.js project

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,3 +8,53 @@ exports.getAllUsers = async (req, res) => {
         res.status(500).send(error.message);
     }
 };
+
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            await user.update(req.body);
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            await user.destroy();
+            res.status(204).send();
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
