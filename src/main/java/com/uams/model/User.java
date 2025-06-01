67fca781d5e51bd50766593d1c80c56d51d8e17a
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-06-01 13:36:58 -0400

    Add comments to User.java

--- src/main/java/com/uams/model/User.java
+++ src/main/java/com/uams/model/User.java
@@ -1,16 +1,27 @@
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../config/database');
 
+/**
+ * User model representing user data.
+ */
 class User extends Model {}
 
 User.init(
   {
+    /**
+     * Unique identifier for each user.
+     * Automatically increments with each new user.
+     */
     userId: {
       type: DataTypes.BIGINT,
       autoIncrement: true,
       primaryKey: true,
       field: 'user_id',
     },
+    /**
+     * Email address of the user.
+     * Must be unique and a valid email format.
+     */
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -20,6 +31,10 @@ User.init(
         isEmail: { msg: 'Please provide a valid email address' },
       },
     },
+    /**
+     * First name of the user.
+     * Cannot be null.
+     */
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -28,6 +43,10 @@ User.init(
         notNull: { msg: 'First name is required' },
       },
     },
+    /**
+     * Last name of the user.
+     * Cannot be null.
+     */
     lastName: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -36,10 +55,18 @@ User.init(
         notNull: { msg: 'Last name is required' },
       },
     },
+    /**
+     * Mobile number of the user.
+     * Can be null.
+     */
     mobileNumber: {
       type: DataTypes.STRING,
       field: 'mobile_number',
     },
+    /**
+     * Password for user authentication.
+     * Cannot be null and must be at least 6 characters long.
+     */
     password: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -62,7 +89,10 @@ User.init(
 
 const Address = require('./address'); // Assuming Address model is defined similarly
 
-// Define the many-to-many relationship
+/**
+ * Establishes a many-to-many relationship between User and Address.
+ * Creates a junction table named 'user_address' with foreign keys: 'user_id' and 'address_id'.
+ */
 User.belongsToMany(Address, {
   through: 'user_address',
   foreignKey: 'user_id',
