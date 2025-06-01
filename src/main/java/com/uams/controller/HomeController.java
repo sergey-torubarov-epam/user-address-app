374917a86e7fcb7042d3fcd7eb9743cd47ac39d1
sergey-torubarov-epam <100948741+sergey-torubarov-epam@users.noreply.github.com>
2025-06-01 13:36:33 -0400

    Add comments to HomeController.java

--- src/main/java/com/uams/controller/HomeController.java
+++ src/main/java/com/uams/controller/HomeController.java
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * HomeController
+ * This controller manages the home route of the application.
+ * It currently has one method - home - which redirects the root URL to the /users URL.
+ */
+
 // Home route - Redirects to /users
 router.get('/', (req, res) => {
+    // This method redirects the root URL to /users when accessed.
     res.redirect('/users');
 });
 
